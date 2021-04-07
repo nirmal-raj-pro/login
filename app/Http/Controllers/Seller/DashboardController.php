0db17e97edbd9c31570ad2e390831fe4c468c923
SPINEBIZ\nirmalrajm <nirmalrajm@spinebiz.com>
2021-04-07 18:12:36 +0530

    Redirection login based on user role, two role admin and serviceprovider/seller

---
+++ app/Http/Controllers/Seller/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth');
+      }
+      
+    Public function index() {
+        return view('seller.dashboard');
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->truncate();
+
+        DB::table('users')->insert([
+            'name' => 'seller',
+            'email' => 'seller@gmail.com',
+            'role' => 'seller',
+            'password' => Hash::make('password'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'role' => 'admin',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
